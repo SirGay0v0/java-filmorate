e72301371258edc5417d37b441f585c237626859
SirGay0v0 <serega112015@gmail.com>
2024-04-02 19:38:09 +0300

    feat: Создать класс, который занимается управлением списком друзей пользователя

---
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -0,0 +1,43 @@
+package ru.yandex.practicum.filmorate.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Service
+public class UserService {
+    private Set<Long> userSet;
+    private Set<Long> friendSet;
+
+    private void makeFriend(User user, User friend) {
+        userSet = user.getFriendsSet();
+        userSet.add(friend.getId());
+        user.setFriendsSet(userSet);
+
+        friendSet = friend.getFriendsSet();
+        friendSet.add(user.getId());
+        friend.setFriendsSet(friendSet);
+    }
+
+    private void unfriend(User user, User friend) {
+        userSet = user.getFriendsSet();
+        userSet.remove(friend.getId());
+        user.setFriendsSet(userSet);
+
+        friendSet = friend.getFriendsSet();
+        friendSet.remove(user.getId());
+        friend.setFriendsSet(friendSet);
+    }
+
+    private Set<Long> getMutualsFriendSet(User user, User friend) {
+        userSet = user.getFriendsSet();
+        friendSet = friend.getFriendsSet();
+        return userSet.stream()
+                .filter(u -> friendSet.contains(u))
+                .collect(Collectors.toSet());
+    }
+}

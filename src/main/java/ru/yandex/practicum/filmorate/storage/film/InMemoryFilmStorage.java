69a00773e0e5a45d99f0793ac6d879c50aa143fd
SirGay0v0 <serega112015@gmail.com>
2024-04-03 21:48:15 +0300

    CleanUp code

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/InMemoryFilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/InMemoryFilmStorage.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.stream.Collectors;
 
 @Component
 public class InMemoryFilmStorage implements FilmStorage {

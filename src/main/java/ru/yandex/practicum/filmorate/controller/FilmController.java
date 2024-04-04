4601ffe5f30cc4bc023987e8213a7b6afa5e41a9
SirGay0v0 <serega112015@gmail.com>
2024-04-04 15:44:57 +0300

    refactor: Убрать хранение промежуточных состояний в FilmService
    refactor: Убрать взаимодействие контроллера с хранилищем

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -15,52 +15,41 @@ import java.util.List;
 @RestController
 public class FilmController {
 
-    private final InMemoryFilmStorage storage;
     private final FilmService service;
 
     @Autowired
     public FilmController(InMemoryFilmStorage storage, FilmService service) {
-        this.storage = storage;
         this.service = service;
     }
 
     @PostMapping("/films")
 
     private Film createFilm(@RequestBody @Valid Film film) {
-        storage.create(film);
-        log.info("Фильм c id " + film.getId() + " создан");
-        return film;
+        return service.createFilm(film);
     }
 
     @PutMapping("/films")
     private Film updateFilm(@RequestBody @Valid Film film) throws ValidationException {
-        if (storage.update(film).equals(film)) {
-            log.info("Фильм c id " + film.getId() + " обновлен");
-            return film;
-        } else throw new ValidationException("not exist");
+        return service.updateFilm(film);
     }
 
     @GetMapping("/films")
     private List<Film> getFilms() {
-        log.info("Возвращен список всех фильмов");
-        return storage.returnAll();
+        return service.getAllFilms();
     }
 
     @PutMapping("/films/{id}/like/{userId}")
     private void likeFilm(@PathVariable long id, @PathVariable long userId) {
-        log.info("Пользователь с id " + userId + " поставил лайк фильму с id " + id);
         service.addLike(userId, id);
     }
 
     @DeleteMapping("/films/{id}/like/{userId}")
     private void removeLike(@PathVariable long id, @PathVariable long userId) {
-        log.info("Пользователь с id " + userId + " убрал лайк с фильма с id " + id);
         service.removeLike(userId, id);
     }
 
     @GetMapping("/films/popular")
     private List<Film> getMostLikableFilms(@RequestParam(defaultValue = "10") String count) {
-        log.info("Возвращен список из " + count + " фильмов с наибольшем коичеством лайков");
         return service.getMostLikableFilmsSet(Integer.parseInt(count));
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -1,44 +1,66 @@
 package ru.yandex.practicum.filmorate.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.exceptions.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
 import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 
 import java.util.Comparator;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Service
 public class FilmService {
-    private final FilmStorage storage;
-    private Set<Long> filmLikesSet;
+    private final FilmStorage filmStorage;
     private final UserStorage userStorage;
 
     @Autowired
-    public FilmService(FilmStorage storage, UserStorage userStorage) {
-        this.storage = storage;
+    public FilmService(FilmStorage filmStorage, UserStorage userStorage) {
+        this.filmStorage = filmStorage;
         this.userStorage = userStorage;
     }
 
     public void addLike(long userId, long filmId) {
-        filmLikesSet = storage.getFilmById(filmId).getLikesUsersSet();
-        filmLikesSet.add(userStorage.getUserById(userId).getId());
-        storage.getFilmById(filmId).setLikesUsersSet(filmLikesSet);
+        filmStorage.getFilmById(filmId).getLikesUsersSet().add(userStorage.getUserById(userId).getId());
+        log.info("Пользователь с id " + userId + " поставил лайк фильму с id " + filmId);
     }
 
     public void removeLike(long userId, long filmId) {
-        filmLikesSet = storage.getFilmById(filmId).getLikesUsersSet();
-        filmLikesSet.remove(userStorage.getUserById(userId).getId());
-        storage.getFilmById(filmId).setLikesUsersSet(filmLikesSet);
+        filmStorage.getFilmById(filmId).getLikesUsersSet().remove(userStorage.getUserById(userId).getId());
+        log.info("Пользователь с id " + userId + " убрал лайк с фильма с id " + filmId);
+
     }
 
     public List<Film> getMostLikableFilmsSet(int count) {
-        return storage.returnAll().stream()
+        log.info("Возвращен список из " + count + " фильмов с наибольшем коичеством лайков");
+        return filmStorage.returnAll().stream()
                 .sorted(Comparator.comparing(Film::sizeOfLikes).reversed())
                 .limit(count)
                 .collect(Collectors.toList());
     }
+
+    public Film createFilm(Film film) {
+        log.info("Фильм c id " + film.getId() + " создан");
+        filmStorage.create(film);
+        return film;
+    }
+
+    public Film updateFilm(Film film) throws ValidationException {
+        if (filmStorage.update(film).equals(film)) {
+            log.info("Фильм c id " + film.getId() + " обновлен");
+            return film;
+        } else throw new ValidationException("not exist");
+    }
+
+    public List<Film> getAllFilms() {
+        log.info("Возвращен список всех фильмов");
+        return filmStorage.returnAll();
+    }
+
+
 }
+
